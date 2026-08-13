26a96122b1a9d23d173267531e037fef77225bcc
ChinhQuy29 <quychinh2004@gmail.com>
2026-08-13 16:05:14 +0700

    Time: 0 ms (100%), Space: 19.3 MB (61.43%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            if i == 1:
+                dp[i] = max(nums[i], dp[i - 1])
+            else:
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        
+        return dp[-1]
